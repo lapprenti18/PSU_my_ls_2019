766a2c09a6d688edc6361a14e15cf8d31cbc6428
emeryck <pokspid@gmail.com>
2020-01-08 16:13:03 +0100

    add: ls ok

--- just_ls.c
+++ just_ls.c
@@ -22,10 +22,8 @@ void just_ls(void)
 
 int file_and_error(char **av, int a, struct stat size_buff)
 {
-    if (lstat(av[a], &size_buff) == 0) {
-        my_putstr(av[a], 0, 1);
+    if (lstat(av[a], &size_buff) == 0)
         return (0);
-    }
     else if (lstat(av[a], &size_buff) == -1) {
         perror("Error");
         return (1);
@@ -37,7 +35,7 @@ void print(DIR *dp, struct dirent *dr, int test)
 {
     char *str;
 
-    while (test == 2 && (dr = readdir(dp)) != NULL) {
+    while ((dr = readdir(dp)) != NULL) {
         str = dr->d_name;
         if (str[0] != '.')
             my_putstr(str, 0, 1);
@@ -51,16 +49,39 @@ int ls_directory(int ac, char **av)
     struct stat size_buff;
     int error = 0;
     int test = 0;
+    int a;
 
     for (int a = 1; a != ac; a += 1) {
         test = file_and_error(av, a, size_buff);
         if (test == 1)
             error = 84;
-        if (test != 2)
+        if (test != 1)
             dp = opendir(av[a]);
-        print(dp, dr, test);
+        if (dp)
+            print(dp, dr, test);
+        if (!dp && test != 1)
+            my_putstr(av[a], 0, 1);
         if (a + 1 < ac)
             write(1, "\n", 1);
     }
     return (error);
 }
+
+// int ls_directory(int ac, char **av)
+// {
+//     DIR *dp;
+//     struct dirent *dr;
+//     char *str;
+
+//     for (int a = 1; a != ac; a += 1) {
+//         dp = opendir(av[a]);
+//         while ((dr = readdir(dp)) != NULL) {
+//             str = dr->d_name;
+//             if (str[0] != '.')
+//                 my_putstr(str, 0, 1);
+//         }
+//         if (a + 1 < ac)
+//             write(1, "\n", 1);
+//     }
+//     return (0);
+// }
